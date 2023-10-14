c8b133f68c6652e2a1412f0d1800e07461302d0c
Júlio César Prado de Moraes <juliopdemoraes@gmail.com>
2023-10-14 11:47:39 -0300

    rocket Commit

---
+++ src/main/java/br/com/juio/todolist/task/iTaskRepository.java
@@ -0,0 +1,12 @@
+package br.com.juio.todolist.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+
+public interface iTaskRepository extends JpaRepository<TaskModel, UUID> {
+    List<TaskModel> findByIdUser(UUID idUser);
+    
+}
---
+++ src/main/java/br/com/juio/todolist/user/iUserRepository.java
@@ -0,0 +1,11 @@
+package br.com.juio.todolist.user;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface iUserRepository extends JpaRepository<UserModel, UUID> {
+    UserModel findByUsername(String username);
+}
